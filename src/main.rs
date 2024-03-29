d2adba8d182a56b27794d416554c54fc01a604ba
Filip Laurentiu <f.laurentiu26@gmail.com>
2024-03-29 16:44:35 +0200

    Use type U128 instead of UnsignedInteger<2>

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use lambdaworks_math::cyclic_group::IsGroup;
-use lambdaworks_math::elliptic_curve::{short_weierstrass::curves::bls12_381::curve::BLS12381Curve, traits::IsEllipticCurve};
-use lambdaworks_math::unsigned_integer::element::UnsignedInteger;
+use lambdaworks_math::elliptic_curve::{
+    short_weierstrass::curves::bls12_381::curve::BLS12381Curve, traits::IsEllipticCurve,
+};
+use lambdaworks_math::unsigned_integer::element::U128;
 
 /*
 Using lambdaworks, compute the public key associated with the secret key 0x6C616D6264617370 with the BLS12-381 elliptic curve.
  */
 fn main() {
-    let priv_key: UnsignedInteger<2> = UnsignedInteger::from("0x6C616D6264617370");
+    let priv_key: U128 = U128::from("0x6C616D6264617370");
     let G = BLS12381Curve::generator();
     let pub_key = G.operate_with_self(priv_key).to_affine();
     println!("Pub key:");
